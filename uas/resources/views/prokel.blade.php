e6c3f5f20a94a186c6f0b932fcfd0b5d4d8ea6c4
rismayvm107 <rismayulia34@upi.edu>
2022-12-20 22:13:02 +0700

    uasPromnet

---
+++ uas/resources/views/prokel.blade.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Company Profile</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+      integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+      crossorigin="anonymous"
+    />
+    <!-- ===== Iconscout CSS ===== -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"
+    ></script>
+    <!-- ===== CSS ===== -->
+    <link rel="stylesheet" href="style.css">
+         
+  </head>
+  <body>
+
+    <!-- navigasi -->
+    <nav
+      class="navbar navbar-expand-lg navbar-light bg-light shadow-lg fixed-top"
+    >
+      <div class="container">
+        <a class="navbar-brand" href="#">GROOMING PET</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse text-right" id="navbarText">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="#Service">Service</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#About Us">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#Gallery">Gallery</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#Book Now">Book Now</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  
+    <!--Banner-->
+    <div class="container-fluid banner">
+      <div class="container">
+        <div class="rounded float-end" >
+        <img src="img/victor-grabarczyk-N04FIfHhv_k-unsplash.jpg" class="img-fluid" alt="" style="width: 430px;" >
+        </div>
+        <h1  style="color: black">
+          GROOMING PET
+        </h1>
+        <p class="text-body col-6">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+        </p>
+      </div>
+    </div>
+
+     <!--Content-->
+     <div class="container-rounded content">
+      <div class="container">
+        <div class="rounded float-end" >
+          <img src="img/pngtree-rabbit-illustration-design-png-image_2327369.jpg" class="img-fluid" alt="" style="width: 300px;" >
+        </div>
+        <h1  style="color: black">
+          GROOMING PET
+        </h1>
+        <p class="text-body col-6">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+        </p>
+        <a href="#Service">
+          <button type="button" class="btn btn-danger btn-lg">
+            Service
+          </button>
+        </a>
+      </div>
+    </div>
+
+    <!--Service-->
+    <div class="container-fluid service pt-5 pb-5">
+      <div class="container text-center">
+        <h2 class="display-3" id="Service">Service</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate,
+          doloribus.
+        </p>
+        <div class="row pt-4">
+          <div class="col-md-4">
+      
+            <img src="img/images (1).png" alt="">
+            <h3 class="mt-3">DOG</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat,
+              a!
+            </p>
+          </div>
+
+          <div class="col-md-4">
+            <img src="img/download.png" alt="">
+            <h3 class="mt-3">CAT</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat,
+              a!
+            </p>
+          </div>
+
+          <div class="col-md-4">
+            <img src="img/images.png" alt="">
+            <h3 class="mt-3">RABBIT</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat,
+              a!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- about us -->
+    <div class="container-fluid pt-5 pb-5">
+      <div class="container">
+        <h2 class="display-3 text-center" id="About Us">About Us</h2>
+        <p class="text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, ex!
+        </p>
+        <div class="clearfix pt-5">
+          <img
+            src="https://img.freepik.com/free-vector/about-us-website-banner-concept-with-thin-line-flat-design_56103-96.jpg?size=626&ext=jpg"
+            class="col-md-6 float-md-end mb-3 crop-img"
+            width="300"
+            height="300"
+          />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+            veritatis at voluptate commodi officiis sapiente.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+            veritatis at voluptate commodi officiis sapiente.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+            veritatis at voluptate commodi officiis sapiente.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Dignissimos laboriosam excepturi exercitationem qui expedita, ex
+            temporibus natus necessitatibus accusamus voluptatibus.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <!--Gallery-->
+    <section class="gallery">
+      <div class="container">
+        <h2 class="display-3 text-center" id="About Us">Gallery</h2>
+        <img src="img/kabo-p6yH8VmGqxo-unsplash.jpg"><img src="img/kabo-p6yH8VmGqxo-unsplash.jpg"> 
+        <img src="img/kabo-p6yH8VmGqxo-unsplash.jpg"><img src="img/kabo-p6yH8VmGqxo-unsplash.jpg"> 
+      </div>
+    </section>
+
+   
+    <!--BAGIAN BOOK NOW-->
+    <div class="container-fluid pt-5 pb-5 Book Now">
+    <div class="container">
+    <h2 class="display-3" id="Book Now">Book Now</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, porro.
+    </p>
+    <!--Content-->
+    <div class="container-rounded content">
+      <div class="container">
+        <div class="rounded float-end" >
+          <img src="img/pngtree-rabbit-illustration-design-png-image_2327369.jpg" class="img-fluid" alt="" style="width: 500px;" >
+        </div>
+      <form action="https://formspree.io/f/mrgdrdnz" method="POST" >
+          <input type="text" name="Name" placeholder="Full Name" required >
+          <input type="email" name="Email" placeholder="Email" required>
+          <input type="address" name="Address" placeholder="Address" required>
+          <select name="Continent">
+              <option>Indonesian</option>
+              <option>English</option>
+          </select>
+          <textarea name="Message" placeholder="Message" required></textarea>
+          <button type="submit">Send</button>
+      </form>
+     </div>
+    </div>
+  
+    
+</body>
+</html>
---
+++ uas/resources/views/tugasbesar.blade.php
@@ -0,0 +1,277 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="remixicons/fonts/remixicon.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+  <title>Grooming pet - Bootstrap Template</title>
+</head>
+
+<body id="home" data-bs-spy="scroll" data-bs-target=".navbar">
+
+  <!-- NAVBAR -->
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+    <div class="container">
+     <a class="navbar-brand" href="#"><img src="img/lgo1.png" alt="" style="width: 65px;"></a>
+      <a class="navbar-brand" href="#">Moy's Pet</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#tentang">Tentang</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#layanan">Layanan</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#galeri">Galeri</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#timkami">Tim Kami</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="#pesan">Pesan</a>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- // NAVBAR -->
+
+
+
+  <!-- HOME -->
+  <section id="hero">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <h1 class="display-3">Let your hair do the talking.</h1>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- // HOME -->
+
+
+  <!-- TENTANG -->
+  <section id="tentang">
+    <div class="container" >
+      <div class="row align-items-center" >
+        <div class="col-lg-5">
+          <img src="./img/bout.jpeg" alt="">
+        </div>
+        <div class="col-lg-4 offset-lg-1">
+          <h6>Tentang</h6>
+          <h1>Tentang Moy's Pet</h1>
+          <p style= "color: #fff;">Grooming hewan adalah praktik membersihkan, menata, dan merawat hewan peliharaan anda. Perawatan ini adalah bertujuan agar hewan peliharaan anda tetap bersih dan sehat. </p>
+          <p style="color: #fff">Perawatan rutin tidak hanya membuat hewan peliharaan anda terlihat bagus tapi juga membantu menjaga kulit dan bulunya tetap sehat dengan menghilangkan rambut mati dan kotoran lainnya, Proses grooming hewan bervariasi tergantung pada usia dan jenisnya, serta gaya perawatan hewan yang ingin dimiliki. Tujuan utama dari grooming hewan peliharaan adalah untuk membuat terlihat baik secara penampilan. .</p>
+          <img class="signature" src="img/my1.jpeg" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- // TENTANG KAMI-->
+
+
+
+  <!-- LAYANAN -->
+  <section id="layanan">
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-12 intro">
+          <h1>Layanan</h1>
+          <h6>Pelayanan</h6>
+          <p style="color: white">Pelayanan yang telah kami sediakan diantara sebagai berikut</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 col-md-6">
+          <div class="service">
+            <img src="./img/pr1.jpg" alt="">
+            <div class="content">
+
+              <h3 class="h5">Paket Anjing</h3>
+              <p style="color: white"> Terdapat 2 paket grooming, yaitu paket grooming standar dan paket grooming lengkap. Setiap paket berbeda-beda dalam pembiayaan. Anda dapat membayar cash langsung ditempat pada saat tim kami kelokasi anda & kami tidak menerima pembayaran online dari bank manapun </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="service">
+            <img src="./img/pr2.jpg" alt="">
+            <div class="content">
+
+              <h3 class="h5">Paket Kucing</h3>
+              <p style="color: white">Terdapat 2 paket grooming, yaitu paket grooming standar dan paket grooming lengkap. Setiap paket berbeda-beda dalam pembiayaan. Anda dapat membayar cash langsung ditempat pada saat tim kami kelokasi anda & kami tidak menerima pembayaran online dari bank manapun </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="service">
+            <img src="./img/pr3.jpg" alt="">
+            <div class="content">
+
+              <h3 class="h5">Paket Kelinci</h3>
+              <p style="color: white">Terdapat 2 paket grooming, yaitu paket grooming standar dan paket grooming lengkap. Setiap paket berbeda-beda dalam pembiayaan. Anda dapat membayar cash langsung ditempat pada saat tim kami kelokasi anda & kami tidak menerima pembayaran online dari bank manapun </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+  </section>
+  <!-- // LAYANAN -->
+
+  
+
+  <!-- Galeri -->
+  <section id="galeri">
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-12 intro">
+          <h1 style = "color: pink">Galeri</h1>
+          <h6 style = "color: pink">Dokumentasi</h6>
+        </div>
+      </div>
+      <div class="container pt-4">
+      <div class="row">
+      <div class="col-4">
+          <div class="card">
+              <img src="img/a.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title" style="text-align: center;"></h5>
+                <p class="card-text" style="text-align: center;"></p>
+              </div>
+            </div>
+      </div>
+      
+      <div class="col-4">
+          <div class="card">
+              <img src="img/b.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title" style="text-align: center;"></h5>
+                <p class="card-text" style="text-align: center;"></p>
+              </div>
+            </div>
+      </div>
+
+      <div class="col-4">
+          <div class="card">
+              <img src="img/c.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title" style="text-align: center;"></h5>
+                <p class="card-text" style="text-align: center;"></p>
+              </div>
+            </div>
+      </div>
+
+      <div class="col-4">
+          <div class="card">
+              <img src="img/d.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title" style="text-align: center;"></h5>
+                <p class="card-text" style="text-align: center;"></p>
+              </div>
+            </div>
+      </div>
+
+      <div class="col-4">
+          <div class="card">
+              <img src="img/e.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title" style="text-align: center;"></h5>
+                <p class="card-text" style="text-align: center;"></p>
+              </div>
+            </div>
+      </div>
+
+      <div class="col-4">
+          <div class="card">
+              <img src="img/f.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title" style="text-align: center;"></h5>
+                <p class="card-text" style="text-align: center;"></p>
+              </div>
+            </div>
+      </div>
+</section>
+  <!-- // FEATURES -->
+
+  <!-- TIM KAMI  -->
+  <section id="timkami">
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-12 intro">
+          <h1>Tim Kami</h1>
+          <h6>Meet our Team</h6>
+        </div>
+      </div>
+
+      <div class="row text-center">
+        <div class="col-lg-6 col-md-7">
+          <div class="timkami">
+            <img src="./img/wawi.jpeg" alt="">
+            <h5 class="mt-4">Risma Yulia & Najwa Andifa</h5>
+            <p style="color:white">Owner Moy's Pet</p>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-7">
+          <div class="timkami">
+            <img src="./img/ceye.jpeg" alt="">
+            <h5 class="mt-4">AZ.'Ulum & Annisa Agni</h5>
+            <p style="color:white">Owner Moy's Pet</p>
+          </div>
+        </div>
+    </div>
+  </section>
+  <!-- // TIM -->
+
+
+  <!-- PESAN -->
+  <section id="pesan">
+  <div class="container-fluid pt-5 pb-5 pesan text-center">
+    <div class="container">
+    <h2 class="display-3" id="pesan" style="color: palevioletred">Pesan</h2>
+    <p style="color:black">
+      silahkan masukan pesan anda untuk melakukan reservasi dibawah ini
+    </p>
+ 
+    <!--Content-->
+    <div class="container-rounded content">
+      <div class="container">
+        <div class="rounded float-end" >
+          <img src="img/logo.jpeg" class="img-fluid" alt="" style="width: 600px;" >
+        </div>
+      <form action="https://formspree.io/f/xpzearzn" method="POST" >
+          <input type="text" name="Nama" placeholder="Nama Lengkap" required >
+          <input type="email" name="Email" placeholder="Email" required>
+          <input type="Alamat" name="Alamat" placeholder="Alamat" required>
+          <input type="datetime-local">
+          <textarea name="Pesan" placeholder="Pesan" required></textarea>
+          <button style="color:white" type="submit">Kirim</button>
+      </form>
+     </div>
+    </div>
+  </section>
+
+  <br/>
+  <footer class="bg-light text-center text-lg-start">
+    <!-- Copyright -->
+      <div class="text-center p-3" style="background-color:palevioletred;">
+       © 2022 Media Sosial Instagram:
+        <a class="text-dark" href="https://instagram.com/moys_pet?igshid=MTg0ZDhmNDA=">Moy'sPet</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
